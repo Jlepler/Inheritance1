f8c17b5b9066b9c9d4d2700d6f9a8c3a364ff8ac
jlepler <jlepler@montgomerycollege.edu>
2019-06-27 09:37:16 -0400

    Initial commit

---
+++ src/Dog.java
@@ -0,0 +1,22 @@
+public class Dog extends Animal {
+
+    public Dog() {
+
+        System.out.println("Now I am a dog!");
+    }
+
+        @Override
+        public String sleep(){
+            return "A dog sleeps...";
+        }
+
+        @Override
+        public String eat(){
+            return "A dog eats...";
+        }
+
+        public String bark(){
+            return "bark!bark!...";
+        }
+
+}
